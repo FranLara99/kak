03fec64ba5ce56a53c7e516e4e910f521aaa7414
Fran Lara Aguilar <flaragu1310@g.educaand.es>
2021-09-22 09:49:24 +0200

    Colores definidos

--- HolaMundo.java
+++ HolaMundo.java
@@ -3,6 +3,7 @@ public class HolaMundo
 	public static void main (String [] args)
 	{
 		//Hola Mundo tags 1.1 ampliado
+		//Añadiendo colores
 		System.out.println("Hola Mundo");
 		System.out.println("Caca");
 	}
